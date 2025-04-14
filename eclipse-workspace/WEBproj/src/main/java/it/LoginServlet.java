5568ec4444d92b7d9739ac170b2656f98adcc7f8
tom <tommaso.domenicali@mail.polimi.it>
2025-04-14 21:28:34 +0200

    [Hopefully stable] version
    maybe not so stable

---
+++ eclipse-workspace/WEBproj/src/main/java/it/LoginServlet.java
@@ -0,0 +1,32 @@
+package it;
+
+import java.io.*;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String type = request.getParameter("tipo");
+        
+        //login hardcoded until sql
+        if (email.equals("docente@polimi.it") && password.equals("tette") && type.equals("docente")){
+            HttpSession session = request.getSession();
+            session.setAttribute("utente", "docente");
+            response.sendRedirect("lecturer.html");
+        } else {
+            response.sendRedirect("index.html");
+        }
+    }
+}
